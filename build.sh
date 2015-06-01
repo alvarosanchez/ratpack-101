d0534a77abf5e161f768ca911f5a839663382f6f
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-06-01 13:15:33 +0200

    Single file

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+git checkout gh-pages
+git merge master
+
 for adoc in `find . -name "*.adoc"`
 do
 	cd `dirname $adoc`
 	asciidoctor `basename $adoc`
 	cd $OLDPWD
 done
+
+cp README.html index.html 
